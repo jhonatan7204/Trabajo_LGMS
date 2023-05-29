7a3766b81cb78577d8c522f97fbf8a7d5feefba6
Mangel <miguelangelmartinezguijarro@gmail.com>
2023-05-29 20:25:21 +0200

    Modificaciones 1.3.1

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+/*
 const inicio = document.querySelector('.titulo-encabezado');
 
 inicio.addEventListener("click", saludar);
@@ -5,7 +7,7 @@ inicio.addEventListener("click", saludar);
 function saludar(){
     alert("hola mundo");
 
-}
+} */
 
 class Persona {
     constructor(dni, nombre, edad) {
@@ -38,6 +40,11 @@ class Persona {
         this._edad = nueva_Edad;
     }
 
+    mostrarDatos(){
+        console.log("DNI -> " + this.dni);
+        console.log("Nombre -> " + this.nombre);
+        console.log("Edad -> " + this.edad);
+    }
 
 }
 
@@ -68,4 +75,112 @@ class Profesor extends Persona {
     get listaAlumnos() {
         return this._listaAlumnos;
     }
+
+    mostrarDatos(){
+        super.mostrarDatos();
+        console.log("Asignatura -> " + this.asignatura);
+        console.log("Clase asignada -> " + this.claseAsignada);      
+    }
 }
+
+class Alumno extends Persona {
+    constructor(dni, nombre, edad, curso, tablet) {
+        super(dni, nombre, edad);
+        this._tablet = tablet; 
+        this._curso = curso;
+    }
+
+    get curso() {
+        return this._curso;
+    }
+
+    set curso(nuevoCurso) {
+        this._curso = nuevoCurso;
+    }
+
+    get tablet() {
+        return this._tablet;
+    }
+
+    set tablet(nuevaTablet) {
+        this._tablet = nuevaTablet;
+    }
+
+    mostrarDatos() {
+        super.mostrarDatos();
+        console.log("Curso -> " + this.curso);
+        console.log("Tablet asignada -> " + this.tablet);      
+    }
+}
+
+class Tablet{
+    constructor(idTablet,alumnoActual,historialIncidencias) {
+        this.idTablet = idTablet;
+        this.alumnoActual = alumnoActual;
+        this.historialIncidencias = historialIncidencias;
+    }
+
+    get idTablet(){
+        return this._idTablet;
+    }
+
+    set idTablet(nuevaTablet) {
+        this._idTablet = nuevaTablet;
+    }
+
+    get alumnoActual() {
+        return this._alumnoActual;
+    }
+
+    set alumnoActual (nuevoAlumno) {
+        this._alumnoActual = nuevoAlumno;
+    }
+
+    get historialIncidencias(){
+        return this._historialIncidencias;
+    }
+
+    set historialIncidencias(historial){
+        this._historialIncidencias = historial;
+    }
+
+    agregarIncidencia(nuevaIncidencia){
+        this.historialIncidencias.push(nuevaIncidencia);
+    }
+
+    mostrarTablet(){
+        console.log("ID tablet -> " + this.idTablet);
+        console.log("Alumno propietario -> " + this.alumnoActual.nombre);
+        console.log("Historial de incidencias: ");
+
+        if (this.historialIncidencias === 0){
+            console.log("La tablet no tiene ningúna incidencia previa");
+        } else {
+            let indice = 1;
+            console.log("");
+            for(let incidencia of this.historialIncidencias) {
+                console.log("Incidencia número " + indice);
+                console.log(incidencia);
+                indice += 1;
+            }
+        }
+    }
+
+
+}
+
+
+const alumno = new Alumno("12345678", "Andrés", 16, "1ºDAM", "Tablet1");
+const historialIncidencias = ["Escupitajo", "Destrozo", "Se descargó tik tok"];
+
+
+const tablet = new Tablet("Tablet1", alumno, historialIncidencias);
+
+tablet.historialIncidencias.agregarIncidencia("afasdfas");
+
+tablet.mostrarTablet();
+
+/*
+let alumno1 = new Alumno("74394272J","Andres",33,"1ºDAN","Tablet_ejemplo");
+
+alumno1.mostrarDatos(); */
