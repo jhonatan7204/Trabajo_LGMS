21075430efd631c73e4b9df4cf2f12b8bdfdf373
AlexRg32 <94568764+AlexRg32@users.noreply.github.com>
2023-06-04 19:35:18 +0200

    Update script_pagAlumno.js

--- pagina alumno/script_pagAlumno.js
+++ pagina alumno/script_pagAlumno.js
@@ -1,7 +1,24 @@
-function mostrarConfirmacion() {
+function mostrarMensajeConfirmacion() {
     
       alert("Formulario enviado correctamente.");
     
   }
 
+  var formulario = document.getElementById('miFormulario');
+  formulario.addEventListener('submit',function(event) {
+    
+        event.preventDefault(); // Evitamos el envío del formulario
+      
+        // Obtenemos los valores de los campos del formulario
+        var nombre = document.getElementById('nombre').value;
+        var modelo = document.getElementById('modelo').value;
+        var incidencia = document.getElementById('incidencia').value;
+      
+        var incidencias = document.getElementById('incidencias');
+        incidencias.textContent = nombre + " " + modelo+ " " + incidencia;
+        this.reset();
+        
+});
+
+  
   
